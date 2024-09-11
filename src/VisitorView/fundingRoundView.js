36e8ace33a9acc9acb8aea72ce984421fa3e7bc0
Friedrich <132067627+XenoviaaaFritzyyy@users.noreply.github.com>
2024-09-11 14:59:38 +0900

    commas in currencies

--- src/VisitorView/fundingRoundView.js
+++ src/VisitorView/fundingRoundView.js
@@ -97,11 +97,12 @@ function FundingRoundView() {
                   <Grid item xs={12}>
                     <Grid container spacing={3}>
                     <Grid item xs={12}>
-                        <Typography variant="body1">
-                          To invest in our company, a minimum purchase of 10,000 shares is required. This ensures a meaningful investment and supports our long-term growth.
-                          <br /><br />
-                          For more information, please contact us.
-                         </Typography>
+                    <Typography variant="body1">
+                      To invest in our company, a minimum purchase of {fundinground.moneyRaisedCurrency} {parseInt(fundinground.minimumShare, 10).toLocaleString()} shares 
+                      is required. This ensures a meaningful investment and supports our long-term growth.
+                      <br /><br />
+                      For more information, please contact us.
+                    </Typography>
                       </Grid>
 
                       <Grid item xs={4}>
@@ -126,13 +127,13 @@ function FundingRoundView() {
 
                       <Grid item xs={4}>
                         <Typography><strong>Money Raised</strong></Typography>
-                        <Typography variant="body1">{fundinground.moneyRaisedCurrency} {fundinground.moneyRaised}</Typography>
+                        {fundinground.moneyRaisedCurrency} {fundinground.moneyRaised ? Number(fundinground.moneyRaised).toLocaleString() : 'N/A'}
                       </Grid>
 
                       <Grid item xs={4}>
                         <Typography><strong>Pre-Money Valuation</strong></Typography>
                         <Typography variant="body1">
-                        {fundinground.moneyRaisedCurrency} {fundinground.preMoneyValuation ? fundinground.preMoneyValuation : 'N/A'}
+                        {fundinground.moneyRaisedCurrency} {fundinground.preMoneyValuation ? Number(fundinground.preMoneyValuation).toLocaleString() : 'N/A'}
                         </Typography>
                       </Grid>
                     </Grid>
@@ -160,7 +161,9 @@ function FundingRoundView() {
                             {investorDetail.investor.firstName} {investorDetail.investor.lastName}
                           </TableCell>
                           <TableCell sx={{ textAlign: 'center' }}>{investorDetail.title}</TableCell>
-                          <TableCell sx={{ textAlign: 'center' }}>{investorDetail.shares}</TableCell>
+                          <TableCell sx={{ textAlign: 'center' }}>
+                          {fundinground.moneyRaisedCurrency} {Number(investorDetail.shares).toLocaleString()}
+                          </TableCell>
                         </TableRow>
                       ))}
                     </TableBody>
