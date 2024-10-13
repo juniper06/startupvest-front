71f91efab9634278e3fc90705088f2d0cbe04063
hazelyn123 <hazelynbalingcasag123@gmail.com>
2024-10-13 23:15:21 +0800

    invest now dialog with error and success

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Stack, Container, Typography, Box } from '@mui/material';
+import { Container, Typography, Box } from '@mui/material';
 import { keyframes } from '@mui/system';
 import { AccountBalance } from '@mui/icons-material'; 
 
@@ -25,7 +25,7 @@ const spinnerRotation = keyframes`
 
 export default function Loading() {
   return (
-    <Container  maxWidth={false} disableGutters 
+    <Container maxWidth={false} disableGutters 
       sx={{ 
         display: 'flex', 
         flexDirection: 'column', 
@@ -35,7 +35,6 @@ export default function Loading() {
         background: 'linear-gradient(135deg, #004A98, #005d64)', 
         backgroundSize: '300% 300%', 
         animation: `${backgroundAnimation} 8s ease infinite`,
-        borderRadius: 3, 
         boxShadow: 10, 
         padding: 3,
         position: 'absolute',
@@ -49,50 +48,18 @@ export default function Loading() {
         <AccountBalance sx={{ fontSize: '4rem', color: '#ffffff', filter: 'drop-shadow(0px 0px 8px rgba(255, 255, 255, 0.5))' }} />
       </Box>
 
-      <Typography 
-        variant="h4" 
-        sx={{ 
-          mb: 1, 
-          fontWeight: 'bold', 
-          color: '#ffffff', 
-          textAlign: 'center',
-          filter: 'drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.3))' 
-        }}>
+      <Typography variant="h4" sx={{  mb: 1,  fontWeight: 'bold',  color: '#ffffff', textAlign: 'center', filter: 'drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.3))' }}>
         Logging in, please wait...
       </Typography>
       
-      <Typography   variant="body1" 
-        sx={{ 
-          mb: 3, 
-          color: '#ffffff', 
-          textAlign: 'center',
-          maxWidth: '400px', 
-          filter: 'drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.3))' 
-        }} >
-        We are preparing your personalized investment experience...
+      <Typography variant="body1" sx={{ mb: 3, color: '#ffffff', textAlign: 'center', maxWidth: '400px', filter: 'drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.3))' }}>
+        We’re preparing a personalized investment experience for you.
       </Typography>
 
-      <Box 
-        sx={{ 
-          position: 'relative', 
-          width: '80px', 
-          height: '80px', 
-          border: '8px solid transparent', 
-          borderTop: '8px solid #ffffff', 
-          borderRadius: '50%', 
-          animation: `${spinnerRotation} 1.5s linear infinite`,
-          filter: 'drop-shadow(0px 0px 8px rgba(0, 0, 0, 0.5))'
-        }} />
+      <Box sx={{ position: 'relative', width: '80px', height: '80px', border: '8px solid transparent', borderTop: '8px solid #ffffff', borderRadius: '50%', 
+          animation: `${spinnerRotation} 1.5s linear infinite`, filter: 'drop-shadow(0px 0px 8px rgba(0, 0, 0, 0.5))'}} />
 
-      <Typography 
-        variant="caption" 
-        sx={{ 
-          marginTop: 3, 
-          color: '#ffffff',
-          textAlign: 'center' 
-        }}>
-        Please do not close this window.
-      </Typography>
+      <Typography variant="caption" sx={{ marginTop: 3, color: '#ffffff', textAlign: 'center' }}>Please do not close this window.</Typography>
     </Container>
   );
 }
--- src/Dialogs/InvestNowDialog.js
+++ src/Dialogs/InvestNowDialog.js
@@ -1,18 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Button,
-  TextField,
-  Grid,
-  Typography,
-  Box,
-  IconButton,
-} from '@mui/material';
+import axios from 'axios'; 
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Grid, Typography, Box, IconButton, Checkbox, FormControlLabel } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
-import axios from 'axios'; // Import Axios for making HTTP requests
 
 const InvestNowDialog = ({
   open,
@@ -20,13 +9,20 @@ const InvestNowDialog = ({
   pricePerShare = 0,
   companyName = '',
   fundingRound = '',
-  fundingRoundId, // Add this prop to get the fundingRoundId
-  investorId, // Add this prop to get the investorId
+  fundingRoundId,
+  investorId,
 }) => {
   const [shareAmount, setShareAmount] = useState('');
   const [displayShareAmount, setDisplayShareAmount] = useState('');
   const [totalCost, setTotalCost] = useState(0);
   const [errors, setErrors] = useState({});
+  const [successDialogOpen, setSuccessDialogOpen] = useState(false); 
+  const [isChecked, setIsChecked] = useState(false);
+  const [investmentError, setInvestmentError] = useState('');
+
+  const handleCheckboxChange = (e) => {
+    setIsChecked(e.target.checked);
+  };
 
   useEffect(() => {
     const total = shareAmount ? shareAmount * pricePerShare : 0;
@@ -37,6 +33,7 @@ const InvestNowDialog = ({
   const handleClose = () => {
     setShareAmount('');
     setDisplayShareAmount('');
+    setIsChecked('');
     setErrors({});
     onClose();
   };
@@ -66,125 +63,135 @@ const InvestNowDialog = ({
     }
 
     try {
-      // Make the PUT request to update the funding round with the investment details
       const response = await axios.put(
         `${process.env.REACT_APP_API_URL}/funding-rounds/${fundingRoundId}/investment`,
         {
           shares: shareAmount,
-          investorId: investorId, // Use the passed investor ID
+          investorId: investorId,
         },
         {
           headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`, // Assuming you store token in localStorage
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
           },
         }
       );
 
       if (response.status === 200) {
-        console.log('Investment successful', response.data);
-        // Optionally, you can show a success message here
+        setSuccessDialogOpen(true);
+        setInvestmentError('');
       } else {
-        console.error('Investment failed', response);
-        // Optionally, show an error message
+        setInvestmentError('Investment failed. Please try again.');
       }
     } catch (error) {
-      console.error('Error during investment:', error);
-      // Optionally, show an error message
+      setInvestmentError('You currently have a pending investment in this funding round. Please wait for its review and acceptance before making any additional investments.');
     }
+  };
 
-    handleClose(); // Close dialog after confirmation
+  const handleSuccessDialogClose = () => {
+    setSuccessDialogOpen(false);
+    handleClose(); 
   };
 
   return (
-    <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
-      <DialogTitle
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'justify',
-          position: 'relative',
-          mt: 2,
-          ml: 3,
-        }}>
-        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
-          Invest in {companyName}
-        </Typography>
-        <IconButton
-          edge="end"
-          aria-label="close"
-          sx={{ position: 'absolute', right: 15, top: -12, color: '#ED262A' }}
-          onClick={handleClose}>
-          <CloseIcon />
-        </IconButton>
-      </DialogTitle>
+    <>
+      {/* Main Investment Dialog */}
+      <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
+        <DialogTitle
+          sx={{ display: 'flex', alignItems: 'center', justifyContent: 'justify', position: 'relative', mt: 2, ml: 3,}}>
+          <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+            Invest in {companyName}
+          </Typography>
+          <IconButton edge="end" aria-label="close" sx={{ position: 'absolute', right: 15, top: -12, color: '#ED262A' }} onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
 
-      <DialogContent sx={{ pl: 6, pr: 6 }}>
-        <Grid container spacing={1}>
-          {/* Investment Information */}
-          <Grid item xs={12}>
-            <Typography variant="body1" gutterBottom align="justify">
-              You are about to invest in the <strong>{fundingRound}</strong> funding round conducted by{' '}
-              <strong>{companyName}</strong>. The price per share is{' '}
-              <strong>P{Number(pricePerShare).toLocaleString() || '0'}</strong>.
-            </Typography>
-          </Grid>
-
-          {/* Shares Input */}
-          <Grid item xs={12}>
-            <Typography>Shares</Typography>
-            <TextField
-              margin="dense"
-              id="share"
-              type="text"
-              fullWidth
-              variant="outlined"
-              placeholder="e.g., 1,000"
-              value={displayShareAmount}
-              onChange={handleShareAmountChange}
-              helperText={errors.shareAmount}
-              error={!!errors.shareAmount}
-            />
-          </Grid>
-
-          {/* Investment Summary */}
-          <Grid item xs={12}>
-            <Box p={3} sx={{ backgroundColor: '#f5f5f5', borderRadius: 2 }}>
-              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
-                Investment Summary
+        <DialogContent sx={{ pl: 6, pr: 6 }}>
+          <Grid container spacing={1}>
+            {/* Investment Information */}
+            <Grid item xs={12}>
+              <Typography variant="body1" gutterBottom align="justify">
+                You are about to invest in the <strong>{fundingRound}</strong> funding round conducted by{' '}
+                <strong>{companyName}</strong>. The price per share is{' '}
+                <strong>P{Number(pricePerShare).toLocaleString() || '0'}</strong>.
               </Typography>
-              <Typography variant="body2" gutterBottom>
-                You are buying <strong>{shareAmount || 0} shares</strong> at a price of{' '}
-                <strong>P{pricePerShare ? Number(pricePerShare).toLocaleString() : '0'}</strong> per share.
-              </Typography>
-              <Typography variant="h6" color="primary" sx={{ mt: 1 }}>
-                Total cost: P{totalCost ? totalCost.toLocaleString() : '0'}
-              </Typography>
-            </Box>
-          </Grid>
+            </Grid>
 
-          {/* Disclaimer */}
-          <Grid item xs={12}>
-            <Typography variant="caption" align="justify" color="textSecondary" sx={{ mt: 2 }}>
-              * By confirming this investment, you agree to the terms and conditions of the funding round. Please note
-              that investments are subject to risk, and you should only invest what you can afford to lose.
-            </Typography>
-          </Grid>
-        </Grid>
-      </DialogContent>
+            {/* Shares Input */}
+            <Grid item xs={12}>
+              <Typography>Shares</Typography>
+              <TextField margin="dense" id="share" type="text" fullWidth variant="outlined" placeholder="e.g., 5" value={displayShareAmount}
+                onChange={handleShareAmountChange} helperText={errors.shareAmount} error={!!errors.shareAmount} />
+            </Grid>
 
-      {/* Dialog Actions */}
-      <DialogActions sx={{ p: 3, justifyContent: 'center' }}>
-        <Button
-          onClick={handleConfirm}
-          variant="contained"
-          color="primary"
-          sx={{ px: 8 }}
-          disabled={!shareAmount || shareAmount <= 0}
-        >
-          Confirm Investment
-        </Button>
-      </DialogActions>
-    </Dialog>
+            {/* Investment Summary */}
+            <Grid item xs={12}>
+              <Box p={3} sx={{ backgroundColor: '#f5f5f5', borderRadius: 2 }}>
+                <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                  Investment Summary
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  You are buying <strong>{shareAmount || 0} shares</strong> at a price of{' '}
+                  <strong>P{pricePerShare ? Number(pricePerShare).toLocaleString() : '0'}</strong> per share.
+                </Typography>
+                <Typography variant="h6" color="primary" sx={{ mt: 1 }}>
+                  Total cost: P{totalCost ? totalCost.toLocaleString() : '0'}
+                </Typography>
+              </Box>
+            </Grid>
+
+            {/* Terms and Conditions Checkbox */}
+            <Grid item xs={12}>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <FormControlLabel
+                  control={
+                    <Checkbox checked={isChecked} onChange={handleCheckboxChange} sx={{ color: '#1976d2' }}/>
+                  }
+                  label={
+                    <Typography variant="body2" sx={{ color: '#555' }}>I agree to the <strong>Terms and Conditions</strong>.</Typography>
+                  }/>
+              </Box>
+            </Grid>
+
+            {/* Display Investment Error */}
+            {investmentError && (
+              <Grid item xs={12}>
+                <Typography variant="body2" color="error">
+                  {investmentError}
+                </Typography>
+              </Grid>
+            )}
+          </Grid>
+        </DialogContent>
+
+        {/* Dialog Actions */}
+        <DialogActions sx={{ p: 3, justifyContent: 'center' }}>
+          <Button onClick={handleConfirm} variant="contained" color="primary" sx={{ px: 8 }}
+            disabled={!shareAmount || shareAmount <= 0 || !isChecked}>
+            Confirm Investment
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      {/* Success Dialog */}
+      <Dialog open={successDialogOpen} onClose={handleSuccessDialogClose} maxWidth="sm" fullWidth>
+        <DialogTitle sx={{ mt: 2, ml: 3 }}>
+          <Typography variant="h5" sx={{ fontWeight: 'bold' }}>Investment Request Submitted</Typography>
+        </DialogTitle>
+
+        <DialogContent sx={{ pl: 6, pr: 6 }}>
+          <Typography variant="body1" gutterBottom align="justify">
+            Congratulations! You have successfully invested in <strong>{companyName} - {fundingRound}</strong>. The company will reach out to you soon via email with details to finalize your investment. <br/> <br/>Thank you for your trust, and rest assured that your investment is being processed. Please keep an eye on your inbox for updates!
+          </Typography>
+        </DialogContent>
+
+        <DialogActions sx={{ p: 3, justifyContent: 'right' }}>
+          <Button onClick={handleSuccessDialogClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
